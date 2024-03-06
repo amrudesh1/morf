aaa7d23a1782e5f0d94df0737d661bcd2728975c
Amrudesh Balakrishnan <amrudesh.balakrishnan@cred.club>
2024-03-06 13:13:46 +0530

    Fixed Bug Related to Path and Made Code Level Optimisations

--- models/JiraModel.go
+++ models/JiraModel.go
@@ -1,6 +1,7 @@
 package models
 
 type JiraModel struct {
+	JiraHost   string `json:"hostUrl"`
 	FileUrl    string `json:"fileUrl"`
 	SlackToken string `json:"slackToken"`
 	Ticket_id  string `json:"ticket_id"`
